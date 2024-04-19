d9a49a74fe03765a3adbfe6ca1fbe6855661ec43
Ahmad Mubarak Patron <mubarak.ahmad98@outlook.com>
2024-04-19 17:31:19 +0300

    19.04.2024

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@
 
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { name } from "./package.json";
+import { name, peerDependencies } from "./package.json";
 
 export default defineConfig({
   define: {
     global: {},
-    "process.env": {}
+    "process.env": {},
   },
   build: {
     lib: {
@@ -16,7 +16,9 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
       formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
     },
-    rollupOptions: {},
+    rollupOptions: {
+      external: Object.keys(peerDependencies), // Excludes peer dependencies from the bundle.
+    },
     sourcemap: true, // Generates source maps for debugging.
     emptyOutDir: true, // Clears the output directory before building.
   },
